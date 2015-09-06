d67227f5f88ab90eef55da010f2cf090687e1110
Sid Bala <Sid.Bala@Outlook.com>
2015-09-05 22:50:52 -0700

    Working implementation

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
 #!/usr/bin/env node
 
 
-var arpListener = require('arp-listener');
 
+var _ = require('lodash');
+var request = require('request-promise');
+var ChildProcess = require('child-process-promise');
 
 var colors = require('colors');
 var argv = require('optimist')
     .usage('DashHooks'.green.bold.underline + ': ' + 'Monitor an Amazon Dash Button on your local network and send webhooks.')
-    .demand('l')
-    .alias('l', 'Watch')
-    .default('l', true)
-    .describe('l', 'Watch mode - list all ARP requests in your network')
     .demand('i')
     .alias('i', 'interface')
     .describe('i', 'The network interface to bind to')
     .default('i', 'en0')
-    .alias('u', 'url')
-    .describe('u', 'The webhook url to trigger when ')
+    .alias('a', 'address')
+    .describe('a', 'MAC address of your Amazon Dash Button')
+    // .alias('u', 'url')
+    // .describe('u', 'The webhook url to trigger when button is pressed')
+    .alias('c', 'command')
+    .describe('c', 'Command to run when a button press is received')
     .argv
 
- 
-arpListener(argv.i, function(arpData) {
-    if(arpData.sender_ha === 'a0:02:dc:12:37:fa') {
+console.log('DashHooks'.green.underline);
+console.log('Binding to Interface   : ' + argv.i.blue);
 
+if(!argv.a) {
+  console.log('You did not specify a Dash Button address using -a'.yellow);
+  console.log('Starting in Discover mode. Use this mode to determine your Dash Button address'.yellow);
+  console.log('Now listening for ALL probes on your network. Press your button and see if it shows up.'.green);
+  var arpListener = require('arp-listener');
+  arpListener(argv.i, function(arpData) {
+    if (arpData.sender_pa.o1 === 0 && arpData.sender_pa.o2 === 0 && arpData.sender_pa.o3 === 0 && arpData.sender_pa.o4 === 0) {
+      if(arpData.target_ha === '00:00:00:00:00:00') {
+        console.log('Got Probe from: '.green + arpData.sender_ha + ' <- Is this your Dash Button?'.blue);
+      }
+    }
+  });
+} else {
+  console.log('Watching for button    : ' + argv.a.blue);
+
+  if(!argv.c) {
+    console.log('No hook command specified. Use -u to specify one. We will start in log-only mode.');
+  } else {
+      console.log('Call on press          : ' + argv.c.blue);
+  }
+  var arpListener = require('arp-listener');
+  arpListener(argv.i, function(arpData) {
+      if(arpData.sender_ha === argv.a) {
         if (arpData.sender_pa.o1 === 0 && arpData.sender_pa.o2 === 0 && arpData.sender_pa.o3 === 0 && arpData.sender_pa.o4 === 0) {
-    if(arpData.target_ha === '00:00:00:00:00:00') {
-        console.log('Hit'.red.bold + ' from: ' + arpData.sender_ha);
-    }
-
-    // console.log(arpData);
-    }
-    }
-})
+          if(arpData.target_ha === '00:00:00:00:00:00') {
+            console.log('Hit!'.green + ' From: ' + arpData.sender_ha.blue);
+            if(argv.c) {
+                ChildProcess.exec(argv.c)
+                .then(function (result) {
+                    console.log('Hooked!'.green + ' Script exit code: ' + result.childProcess.exitCode);
+                })
+                .fail(function (err) {
+                    console.error('ERROR: '.red + 'Script invocation failed: ' + err);
+                })
+            }
+          }
+        }
+      }
+  })
+}
---
+++ sampleSlackNotiication.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -X POST --data-urlencode 'payload={"channel": "#dashbuttonhooks", "username": "DashButton", "text": "Someone just pressed me", "icon_emoji": ":point_right:"}' https://hooks.slack.com/services/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
